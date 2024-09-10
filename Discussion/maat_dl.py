5d1f01a825521ef8633c861f77e2fc8920e581c8
fairness00 <multifairness@gmail.com>
2024-09-10 15:25:52 +0800

    Add files via upload

---
+++ Discussion/maat_dl.py
@@ -0,0 +1,113 @@
+import sys
+import os
+sys.path.append(os.path.abspath('.'))
+from Measure_new import measure_final_score
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+from numpy import mean
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+from utility import get_classifier, get_data
+from sklearn.model_selection import train_test_split
+import argparse
+import copy
+from WAE import data_dis
+
+from aif360.datasets import BinaryLabelDataset
+import tensorflow as tf
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--dataset", type=str, required=True,
+                    choices = ['adult', 'german', 'compas', 'bank', 'mep'], help="Dataset name")
+parser.add_argument("-c", "--clf", type=str, required=True,
+                    choices = ['dl'], help="Classifier name")
+parser.add_argument("-p", "--protected", type=str, required=True,
+                    help="Protected attribute")
+
+args = parser.parse_args()
+
+scaler = MinMaxScaler()
+dataset_used = args.dataset
+attr = args.protected
+clf_name = args.clf
+
+val_name = "maat_training_{}_{}_{}.txt".format(clf_name,dataset_used,attr)
+fout = open(val_name, 'w')
+
+dataset_orig, privileged_groups,unprivileged_groups = get_data(dataset_used, attr)
+
+results = {}
+performance_index = ['accuracy', 'recall', 'precision', 'f1score', 'mcc', 'srp', 'sru', 'fprp', 'fpru', 'tprp', 'tpru', 'spd',  'aod', 'eod']
+for p_index in performance_index:
+    results[p_index] = []
+
+repeat_time = 20
+
+for r in range(repeat_time):
+    print (r)
+
+    np.random.seed(r)
+    #split training data and test data
+    dataset_orig_train, dataset_orig_test = train_test_split(dataset_orig, test_size=0.3, shuffle=True)
+    dataset_orig_test = copy.deepcopy(dataset_orig_train)
+
+    dataset_orig_train_new = data_dis(pd.DataFrame(dataset_orig_train),attr)
+
+    scaler = MinMaxScaler()
+    scaler.fit(dataset_orig_train)
+    dataset_orig_train = pd.DataFrame(scaler.transform(dataset_orig_train), columns=dataset_orig.columns)
+    dataset_orig_test_1 = pd.DataFrame(scaler.transform(dataset_orig_test), columns=dataset_orig.columns)
+
+    scaler = MinMaxScaler()
+    scaler.fit(dataset_orig_train_new)
+    dataset_orig_train_new = pd.DataFrame(scaler.transform(dataset_orig_train_new), columns=dataset_orig.columns)
+    dataset_orig_test_2 = pd.DataFrame(scaler.transform(dataset_orig_test), columns=dataset_orig.columns)
+
+    dataset_orig_train = BinaryLabelDataset(favorable_label=1, unfavorable_label=0, df=dataset_orig_train,
+                                            label_names=['Probability'],
+                                            protected_attribute_names=[attr])
+    dataset_orig_train_new = BinaryLabelDataset(favorable_label=1, unfavorable_label=0, df=dataset_orig_train_new,
+                                                label_names=['Probability'],
+                                                protected_attribute_names=[attr])
+    dataset_orig_test_1 = BinaryLabelDataset(favorable_label=1, unfavorable_label=0, df=dataset_orig_test_1,
+                                             label_names=['Probability'],
+                                             protected_attribute_names=[attr])
+    dataset_orig_test_2 = BinaryLabelDataset(favorable_label=1, unfavorable_label=0, df=dataset_orig_test_2,
+                                             label_names=['Probability'],
+                                             protected_attribute_names=[attr])
+
+    clf1 = get_classifier(clf_name, dataset_orig_train.features.shape[1:])
+    clf1.fit(dataset_orig_train.features, dataset_orig_train.labels,epochs=20)
+
+    clf2 = get_classifier(clf_name, dataset_orig_train_new.features.shape[1:])
+    clf2.fit(dataset_orig_train_new.features, dataset_orig_train_new.labels,epochs=20)
+
+    test_df_copy = copy.deepcopy(dataset_orig_test_1)
+    pred_de1 = clf1.predict(dataset_orig_test_1.features).reshape(-1, 1)
+    pred_de2 = clf2.predict(dataset_orig_test_2.features).reshape(-1, 1)
+
+    res = []
+    for i in range(len(pred_de1)):
+        prob_t = (pred_de1[i]+pred_de2[i])/2
+        if prob_t >= 0.5:
+            res.append(1)
+        else:
+            res.append(0)
+
+    test_df_copy.labels = np.array(res)
+
+    round_result = measure_final_score(dataset_orig_test_1, test_df_copy, privileged_groups, unprivileged_groups)
+    for i in range(len(performance_index)):
+        results[performance_index[i]].append(round_result[i])
+
+for p_index in performance_index:
+    fout.write(p_index + '\t')
+    for i in range(repeat_time):
+        fout.write('%f\t' % results[p_index][i])
+    fout.write('%f\n' % (mean(results[p_index])))
+fout.close()
